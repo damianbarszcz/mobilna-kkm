20009b2f2bdb2ee7fbdd665ebf7872f536da5b59
Damian Barszcz <mail.damianbarszcz@gmail.com>
2023-03-20 00:36:02 +0100

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SZCZEGÓŁY BILETU</Text>
+
+      <Text>Zakupiony</Text>
+
+      <Text>02.03.2023 22:21</Text>
+
+      <Text>Bilet opłacony</Text>
+
+      <Text>Tak</Text>
+
+      <Text>Typ płatności</Text>
+
+      <Text>ePłatność - przelew elektroniczny</Text>
+
+      <Text>Stan płatności</Text>
+
+      <Text>Płatność potwierdzona i zakończona</Text>
+
+      <Text>Stan transakcji</Text>
+
+      <Text>Transakcja zakończona pomyślnie - nie wymaga zapisu na nośniku plastikowym</Text>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0d48ae',
     alignItems: 'center',
     justifyContent: 'center',
   },
